f54cca45236563a9c6d9335c13cc25ac07b27f0b
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-08 13:59:18 -0800

    Add inputs, add getQuestions function, add to constructor
     Mike Pingel <mdpingel@gmail.com>
    Jonah Johansen <johansenjonah+git@gmail.com>

---
+++ src/js/trivia.js
@@ -0,0 +1,6 @@
+export default class Trivia {
+  constructor() {
+    this.token = "";
+    this.questions = {};
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,98 @@
+import $ from 'jquery';
+import './css/styles.css';
+import Trivia from './js/trivia.js';
+
+function getToken(trivia) {
+  let request = new XMLHttpRequest();
+  const url = 'https://opentdb.com/api_token.php?command=request';
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      trivia.token = response.token;
+      // trivia.saveToken(response);
+    }
+  }
+  request.open("GET", url, true);
+  request.send();
+}
+
+function getQuestions(numQuestions, category, difficulty, trivia) {
+  let request = new XMLHttpRequest();
+  const url = `https://opentdb.com/api.php?amount=${numQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`;
+  console.log(url);
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      trivia.questions = response;
+    }
+  }
+  request.open("GET", url, true);
+  request.send();
+}
+
+
+
+$(document).ready(function() {
+  let trivia = new Trivia();
+  getToken(trivia);
+  
+  $('#testButton').on('click', function(e){
+    e.preventDefault();
+    
+    console.log(trivia.token);
+   
+  });
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    const numQuestions = $('#number').val();
+    const category = $('#category').val();
+    const difficulty = $('#difficulty').val();
+    getQuestions(numQuestions, category, difficulty, trivia);
+    console.log(trivia.questions);
+  });
+  
+});
+
+// {
+//   "response_code": 0,
+//   "results": [
+//       {
+//           "category": "Entertainment: Film",
+//           "type": "multiple",
+//           "difficulty": "easy",
+//           "question": "Who plays Alice in the Resident Evil movies?",
+//           "correct_answer": "Milla Jovovich",
+//           "incorrect_answers": [
+//               "Madison Derpe",
+//               "Milla Johnson",
+//               "Kim Demp"
+//           ]
+//       },
+//       {
+//           "category": "Science: Computers",
+//           "type": "multiple",
+//           "difficulty": "easy",
+//           "question": "Which programming language shares its name with an island in Indonesia?",
+//           "correct_answer": "Java",
+//           "incorrect_answers": [
+//               "Python",
+//               "C",
+//               "Jakarta"
+//           ]
+//       },
+//       {
+//           "category": "Science & Nature",
+//           "type": "multiple",
+//           "difficulty": "easy",
+//           "question": "What is the official name of the star located closest to the North Celestial Pole?",
+//           "correct_answer": "Polaris",
+//           "incorrect_answers": [
+//               "Eridanus",
+//               "Gamma Cephei",
+//               "Iota Cephei"
+//           ]
+//       }
+//   ]
+// }
